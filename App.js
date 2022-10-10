b4a6abd92d33be92ae53724405cf54293b9995f7
Alvin <yanson.alvin.info@gmail.com>
2022-10-10 13:19:10 +0800

    fix

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Button, TextInput, Text } from 'react-native';
+import * as SMS from 'expo-sms';
 
 export default function App() {
+  // define three state variables
+  const [smsServiceAvailable, setSmsServiceAvailable] = React.useState(false);
+  const [phoneNumber, setPhoneNumber] = React.useState('');
+  const [message, setMessage] = React.useState('');
+
+  // trigger the handler method as soon as the function component
+  // SmsScreen is mounted
+  React.useEffect(() => {
+    checkIfServiceAvailable();
+  }, []);
+
+  // define a handler method to check if SMS service is available or not
+
+  const checkIfServiceAvailable = async () => {
+    const isAvailable = await SMS.isAvailableAsync();
+    if (isAvailable) {
+      setSmsServiceAvailable(true);
+    }
+  };
+
+  const onComposeSms = async () => {
+    if (smsServiceAvailable && phoneNumber && message) {
+      await SMS.sendSMSAsync(phoneNumber.toString(), message);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, paddingVertical: 50 }}>
+      {smsServiceAvailable ? (
+        <View style={styles.formController}>
+        <TextInput
+          style={styles.input}
+          value={phoneNumber}
+          onChangeText={text => setPhoneNumber(text)}
+          keyboardType='number-pad'
+          placeholder='Enter phone number here'
+        />
+        <TextInput
+          style={styles.input}
+          value={message}
+          onChangeText={text => setMessage(text)}
+          keyboardType='default'
+          placeholder='Enter message here'
+        />
+        <Button
+          title='Send SMS'
+          onPress={onComposeSms}
+          disabled={!smsServiceAvailable}
+        />
+      </View>
+      ) : (
+        <Text>No SMS service available.</Text>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 50
   },
+  formController: {
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    alignItems: 'center'
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    margin: 12,
+    borderWidth: 1,
+    paddingHorizontal: 10
+  }
 });
